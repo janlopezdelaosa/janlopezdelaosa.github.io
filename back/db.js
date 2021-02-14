14b2848d67346d0177e57a27065223c083ddff7a
janlopezdelaosa <janlopezdelaosa@gmail.com>
2021-02-14 12:44:04 +0100

    Process private key

--- back/db.js
+++ back/db.js
@@ -3,7 +3,7 @@ var firebase = require("firebase");
 firebase.initializeApp({
   serviceAccount: {
     projectId: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   },
   databaseURL: process.env.DATABASE_URL,
