bc293d5504f35decb058cce6b56ce9be8014d213
janlopezdelaosa <janlopezdelaosa@gmail.com>
2021-02-14 12:16:25 +0100

    Add db url as heroku env var

--- back/db.js
+++ back/db.js
@@ -1,12 +1,12 @@
 var firebase = require("firebase");
 
 firebase.initializeApp({
-  credential: admin.credential.cert({
+  serviceAccount: {
     projectId: process.env.FIREBASE_PROJECT_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
-  databaseURL: "https://weather-forecast-3359c-default-rtdb.firebaseio.com/",
+  },
+  databaseURL: process.env.DATABASE_URL,
 });
 
 var db = firebase.database();
